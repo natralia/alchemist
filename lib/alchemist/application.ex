093bc5f1c9f4061932942562cb905694fc0abca9
Nathalia Caetano <nathzcaetano@gmail.com>
2022-09-09 18:29:06 -0300

    initial commit

---
+++ lib/alchemist/application.ex
@@ -0,0 +1,20 @@
+defmodule Alchemist.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Alchemist.Worker.start_link(arg)
+      # {Alchemist.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Alchemist.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
